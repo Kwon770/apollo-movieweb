7bab1b9d0f29741eba5b9991aeea33af938913f9
Kwon770 <sckwon770@gmail.com>
2020-03-11 10:52:40 +0900

    2-0 Local State part One

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,13 @@
 import ApolloClient from "apollo-boost";
 
+// resolvers: Resolve several thing from client (like field)
 const client = new ApolloClient({
-  uri: "http://localhost:4000/"
+  uri: "http://localhost:4000/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    }
+  }
 });
 
 export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,11 +4,13 @@ import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
 import Movie from "../components/Movie";
 
+// Get isLiked also, but only from client (@client / telegraph ql)
 const GET_MOVIES = gql`
   {
     movies {
       id
       medium_cover_image
+      isLiked @client
     }
   }
 `;
@@ -59,7 +61,12 @@ export default () => {
       {loading && <Loading>Loading...</Loading>}
       <Movies>
         {data?.movies?.map(m => (
-          <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          <Movie
+            key={m.id}
+            id={m.id}
+            isLiked={m.isLiked}
+            bg={m.medium_cover_image}
+          />
         ))}
       </Movies>
     </Container>
