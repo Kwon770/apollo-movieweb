443dbae4184f67cc1437a1d50a47ec76b145f712
Kwon770 <sckwon770@gmail.com>
2020-03-11 11:24:03 +0900

    2-3 Conclusion

--- src/apollo.js
+++ src/apollo.js
@@ -4,10 +4,13 @@ import ApolloClient from "apollo-boost";
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   resolvers: {
+    // The name must be same with api (type in db)
     Movie: {
       isLiked: () => false
     },
+    // Mutaion like backend-Mutation
     Mutation: {
+      // root, arguemnt, context
       togglelikeMovie: (_, { id, isLiked }, { cache }) => {
         cache.writeData({
           id: `Movie:${id}`,
